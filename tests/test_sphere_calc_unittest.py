d5c2bf1c825f269e9cfdeb4a5c71c46ffc49f62a
jerellaquino <jerellaquino96@gmail.com>
2024-05-31 20:23:28 -0400

    add more test cases

--- tests/test_sphere_calc_unittest.py
+++ tests/test_sphere_calc_unittest.py
@@ -10,11 +10,19 @@ from foo_package.foo import sphereVolume, sphereSA
 
 class TestSphereCalc(unittest.TestCase):
 
+    # Check different function input types:
+    
     def test_sphereVolume(self):
         self.assertAlmostEqual(sphereVolume(4), 268.08, places=2)
+        self.assertAlmostEqual(sphereVolume(3.14159264), 129.87879, places=5)
+        self.assertEqual(sphereVolume("hi"), 'error: check radius input value, must be integer or float')
+
 
     def test_sphereSA(self):
         self.assertAlmostEqual(sphereSA(2), 50.27, places=2)
+        self.assertAlmostEqual(sphereSA(3.14159264), 124.02511, places=5)
+        self.assertEqual(sphereSA("hi"), 'error: check radius input value, must be integer or float')
+
 
 if __name__ == '__main__':
     unittest.main()
